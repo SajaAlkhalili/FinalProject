26c20027b3beb196f909665fe1ab5096a98e307f
SajaAlkhalili <135491864+SajaAlkhalili@users.noreply.github.com>
2024-02-10 16:20:57 +0200

    Version3

--- app/src/main/java/edu/birzeit/students/finalproject/SplashActivity.java
+++ app/src/main/java/edu/birzeit/students/finalproject/SplashActivity.java
@@ -3,6 +3,7 @@ package edu.birzeit.students.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
 
